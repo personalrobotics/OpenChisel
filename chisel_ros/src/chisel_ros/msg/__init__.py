a53ded7be6355b448710ff43173795fdb4098561
mklingen <mklingen@ash.personalrobotics.ri.cmu.edu>
2014-10-03 13:34:21 -0400

    deleted autogenerated files

--- chisel_ros/src/chisel_ros/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._ChunkMessage import *
-from ._ChunkListMessage import *
-from ._Chunk import *
-from ._ChunkList import *
--- chisel_ros/src/chisel_ros/srv/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._SaveMeshService import *
-from ._PauseService import *
-from ._ResetService import *
-from ._GetAllChunksService import *
