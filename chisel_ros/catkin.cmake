ab8f40dbdba65098100309f4f8f9763cdef73742
mklingen <mklingen@ash.personalrobotics.ri.cmu.edu>
2015-02-26 13:04:02 -0500

    added cmake files

---
+++ chisel_ros/catkin.cmake
@@ -0,0 +1,35 @@
+cmake_minimum_required(VERSION 2.8.3)
+
+find_package(catkin REQUIRED COMPONENTS roscpp std_msgs sensor_msgs geometry_msgs tf open_chisel message_generation)
+find_package(Eigen REQUIRED)
+include_directories(${Eigen_INCLUDE_DIRS})
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++0x")
+
+add_message_files(FILES
+  ChunkMessage.msg
+  ChunkListMessage.msg
+)
+
+add_service_files(FILES
+  GetAllChunksService.srv
+  PauseService.srv
+  ResetService.srv
+  SaveMeshService.srv
+)
+
+generate_messages(DEPENDENCIES std_msgs sensor_msgs geometry_msgs)
+
+catkin_package(CATKIN_DEPENDS roscpp tf message_runtime std_msgs sensor_msgs open_chisel)
+
+
+include_directories(include ${catkin_INCLUDE_DIRS})
+
+add_library(${PROJECT_NAME} src/ChiselServer.cpp)
+target_link_libraries(${PROJECT_NAME} ${catkin_LIBRARIES})
+add_executable(ChiselNode src/ChiselNode.cpp)
+target_link_libraries(ChiselNode ${PROJECT_NAME} ${catkin_LIBRARIES})
+
+install(DIRECTORY include/${PROJECT_NAME}/
+  DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
+  PATTERN ".git" EXCLUDE
+)
---
+++ open_chisel/catkin.cmake
@@ -0,0 +1,34 @@
+cmake_minimum_required(VERSION 2.8.3)
+
+catkin_package(INCLUDE_DIRS include
+               LIBRARIES ${PROJECT_NAME})
+
+find_package(catkin REQUIRED)
+find_package(Eigen REQUIRED)
+include_directories(${Eigen_INCLUDE_DIRS})
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++0x")
+
+include_directories(include)
+
+add_library(${PROJECT_NAME} 
+	src/Chunk.cpp 
+	src/ChunkManager.cpp 
+	src/DistVoxel.cpp  
+	src/ColorVoxel.cpp
+	src/geometry/AABB.cpp
+	src/geometry/Plane.cpp
+	src/geometry/Frustum.cpp
+	src/camera/Intrinsics.cpp
+	src/camera/PinholeCamera.cpp
+	src/pointcloud/PointCloud.cpp
+	src/ProjectionIntegrator.cpp
+	src/Chisel.cpp
+	src/mesh/Mesh.cpp
+	src/marching_cubes/MarchingCubes.cpp
+	src/io/PLY.cpp)
+
+target_link_libraries(${PROJECT_NAME} ${CATKIN_LIBRARIES})
+
+install(DIRECTORY include/${PROJECT_NAME}/
+  DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
+)
