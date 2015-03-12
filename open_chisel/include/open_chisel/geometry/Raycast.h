a91b804ba7277d5f3dc835e2e541faa6dbba40bb
mklingen <mklingen@ash.personalrobotics.ri.cmu.edu>
2015-03-12 17:50:25 -0400

    added missing files

---
+++ open_chisel/include/open_chisel/geometry/Raycast.h
@@ -0,0 +1,11 @@
+#ifndef RAYCAST_H_
+#define RAYCAST_H_
+
+#include "Geometry.h"
+
+float signum(float x);
+float mod(float value, float modulus);
+float intbound(float s, float ds);
+void Raycast(const chisel::Vec3& start, const chisel::Vec3& end, const chisel::Point3& min, const chisel::Point3& max, chisel::Point3List* output);
+
+#endif // RAYCAST_H_ 
