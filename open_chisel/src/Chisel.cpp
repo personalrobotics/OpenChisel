bf9721e10c29ea13d6cc7649c7fb45aa1a16ef03
Marius Schnaubelt <schnaubelt@sim.tu-darmstadt.de>
2016-01-14 20:24:11 +0100

    Improved interpolation

--- open_chisel/src/Chisel.cpp
+++ open_chisel/src/Chisel.cpp
@@ -153,7 +153,7 @@ namespace chisel
     printf("\n \n  ~ %f HZ     \n \n", 1/elapsed_secs);
   }
 
-  void Chisel::IntegrateChunks(const ProjectionIntegrator& integrator, const ChunkMap& chunks)
+  void Chisel::IntegrateChunks(const ProjectionIntegrator& integrator,  ChunkManager& sourceChunkManager)
   {
     clock_t begin = clock();
     printf("CHISEL: Integrating chunks \n");
@@ -162,18 +162,19 @@ namespace chisel
     std::vector<chisel::ChunkPtr> chunksToIntegrate;
 
     float mapResolution = GetChunkManager().GetResolution();
-    float chunkResolution = chunks.cbegin()->second->GetVoxelResolutionMeters();
+    float chunkResolution = sourceChunkManager.GetResolution();
     Eigen::Vector3i mapChunksize = GetChunkManager().GetChunkSize();
-    Eigen::Vector3i chunkChunksize = chunks.cbegin()->second->GetNumVoxels();
+    Eigen::Vector3i chunkChunksize = sourceChunkManager.GetChunkSize();
 
-    ChunkManager tempTargetChunkManager(mapChunksize,mapResolution, chunks.cbegin()->second->HasColors());
+    const bool color = sourceChunkManager.GetUseColor();
+    ChunkManager tempTargetChunkManager(mapChunksize, mapResolution, color);
 
     if(std::fabs(mapResolution - chunkResolution) < 0.0001 && mapChunksize == chunkChunksize)
     {
       printf("Chunk Size and chunk Resolution fit for source and target chunks! \n");
-      for (const std::pair<chisel::ChunkID, ChunkPtr>& c : chunks)
+      for (const std::pair<chisel::ChunkID, ChunkPtr>& c : sourceChunkManager.GetChunks())
       {
-        chunksIntersecting.push_back(c.first);  // c->GetID());
+        chunksIntersecting.push_back(c.first);
         chunksToIntegrate.push_back(c.second);
       }
     }
@@ -182,14 +183,9 @@ namespace chisel
       if (mapResolution != chunkResolution)
       {
         printf("Interpolating chunks! \n");
-        int counter = 0;
 
-      /*}
-      else if (mapResolution < chunkResolution)
-      {*/
-        for (const std::pair<chisel::ChunkID, ChunkPtr>& c : chunks)
+        for (const std::pair<chisel::ChunkID, ChunkPtr>& c : sourceChunkManager.GetChunks())
         {
-
           Vec3 origin = c.second->GetOrigin();
           Vec3 maxChunkPos = origin + chunkResolution * chunkChunksize.cast<float>();
 
@@ -198,13 +194,6 @@ namespace chisel
           Vec3 maxPosition(maxChunkPos(0) - std::fmod(maxChunkPos(0), mapResolution), maxChunkPos(1) - std::fmod(maxChunkPos(1), mapResolution), maxChunkPos(2) - std::fmod(maxChunkPos(2), mapResolution));
 
           ChunkPtr chunk;
-          ChunkManager tempSourceChunkManager(chunkChunksize,chunkResolution, chunks.cbegin()->second->HasColors());
-
-          Vec3 range = (maxPosition-minPosition)/mapResolution;
-         // printf("Visiting %f voxels for this chunk!", range(0)*range(1)*range(2));
-
-          std::vector<std::pair<ChunkID, VoxelID>> cornerVoxels(8);
-          ChunkID sourceChunkID;
           ChunkID targetChunkID;
 
           for (float x = minPosition(0); x < maxPosition(0); x+=mapResolution)
@@ -214,16 +203,8 @@ namespace chisel
               for (float z = minPosition(2); z < maxPosition(2); z+=mapResolution)
               {
                 Vec3 voxelPosition(x,y,z);
-                //printf("Get IDs \n");
-                sourceChunkID = tempSourceChunkManager.GetIDAt(voxelPosition);
                 targetChunkID = tempTargetChunkManager.GetIDAt(voxelPosition);
 
-                //  printf("Get voxel coords \n");
-                const Point3 voxelCoords = c.second->GetVoxelCoords(voxelPosition-origin);
-                // printf("Get voxel corners \n");
-                getVoxelCornerIDs(&cornerVoxels, sourceChunkID, voxelCoords, chunkChunksize);
-
-               // printf("Get target chunk \n");
                 if (tempTargetChunkManager.HasChunk(targetChunkID))
                 {
                   chunk = tempTargetChunkManager.GetChunk(targetChunkID);
@@ -234,30 +215,16 @@ namespace chisel
                   chunk = tempTargetChunkManager.GetChunk(targetChunkID);
                 }
 
-                //Vec3 targetOrigin =  chunk->GetOrigin();
-               // printf("Got origin of target \n");
-                //printf("Origin: %f %f %f \n", targetOrigin(0),targetOrigin(1),targetOrigin(2));
-               // printf("Get dist voxel \n");
-
-                DistVoxel* d = tempTargetChunkManager.GetDistanceVoxelMutable(voxelPosition);
-
-               // printf("Got dist voxel \n");
-
-              //  printf("interpolate voxel");
-                interpolateDistVoxel(voxelPosition, cornerVoxels, chunks, chunkResolution, chunkChunksize, d );
+                interpolateDistVoxel(voxelPosition, sourceChunkManager, tempTargetChunkManager.GetDistanceVoxelMutable(voxelPosition));
               }
             }
           }
-
-          counter++;
-          printf("Finished chunk number %d \n", counter);
         }
-
       }
       else
       {
         //Resulution is fitting, but because of different chunksizes, all data goes into other chunks
-        for (const std::pair<chisel::ChunkID, ChunkPtr>& c : chunks)
+        for (const std::pair<chisel::ChunkID, ChunkPtr>& c : sourceChunkManager.GetChunks())
         {
           ChunkPtr chunk;
 
@@ -298,8 +265,6 @@ namespace chisel
               fprintf(stderr,"VoxelID %d", voxelID);
               Point3 p = chunk->GetVoxelCoords(relPosition);
               fprintf(stderr,"Coordinates %d %d %d \n", p(0), p(1), p(2));
-
-              //exit(0);
             }
           }
         }
@@ -359,7 +324,7 @@ namespace chisel
       }
     //);
     printf("CHISEL: Done with chunks");
-    //GarbageCollect(garbageChunks);DistVoxel
+    //GarbageCollect(garbageChunks);
     //chunkManager.PrintMemoryStatistics();
 
     clock_t end = clock();
@@ -367,73 +332,79 @@ namespace chisel
     printf("\n \n  Time needed: %f  ms  \n \n", elapsed_secs*1000);
   }
 
-  bool Chisel::interpolateDistVoxel(const Vec3& voxelPos, const std::vector<std::pair<ChunkID, VoxelID>>& cornerVoxels, const ChunkMap& chunks, const float& resolution, const Point3& chunkSize, DistVoxel* voxel)
+
+  bool Chisel::interpolateDistVoxel(const Vec3& voxelPos, ChunkManager& sourceChunkManager, DistVoxel* voxel)
   {
     if(voxel == nullptr)
       return false;
 
-      const float x = voxelPos(0);
-      const float y = voxelPos(1);
-      const float z = voxelPos(2);
+    const float x = voxelPos(0);
+    const float y = voxelPos(1);
+    const float z = voxelPos(2);
 
-      Vec3 startPos = resolution * (cornerVoxels[0].first.cwiseProduct(chunkSize)).cast<float>() + resolution * getVoxelCoordinates(cornerVoxels[0].second, chunkSize).cast<float>();
-      Vec3 endPos = resolution * (cornerVoxels[7].first.cwiseProduct(chunkSize)).cast<float>() + resolution * getVoxelCoordinates(cornerVoxels[7].second, chunkSize).cast<float>();
+    ChunkID  startChunkID = sourceChunkManager.GetIDAt(voxelPos);
 
-     /* printf("startPos: %f %f %f \n",  startPos(0), startPos(1), startPos(2));
-      printf("Pos: %f %f %f \n",  voxelPos(0), voxelPos(1), voxelPos(2));
-      printf("endPos: %f %f %f \n \n",  endPos(0), endPos(1), endPos(2));*/
+    if (!sourceChunkManager.HasChunk(startChunkID))
+    {
+      return false;
+    }
 
-      const int x_0 = startPos(0);
-      const int y_0 = startPos(1);
-      const int z_0 = startPos(2);
-      const int x_1 = endPos(0);
-      const int y_1 = endPos(1);
-      const int z_1 = endPos(2);
+    ChunkPtr startChunk = sourceChunkManager.GetChunk(startChunkID);
+    Point3 startVoxelID = startChunk->GetVoxelCoords( voxelPos- startChunk->GetOrigin());
 
-     // printf("Getting distvoxels \n");
-     // clock_t begin = clock();
+    const float resolution = sourceChunkManager.GetResolution();
+    const Point3 chunkSize = sourceChunkManager.GetChunkSize();
 
+    const Vec3 startPos = resolution * (startChunkID.cwiseProduct(chunkSize)).cast<float>() + resolution * startVoxelID.cast<float>();
+    const Vec3 endPos = startPos + resolution * Vec3::Ones();
 
-      const DistVoxel v_000 = getDistVoxelFromMap(cornerVoxels[0].first, cornerVoxels[0].second, chunks);
-      const DistVoxel v_001 = getDistVoxelFromMap(cornerVoxels[1].first, cornerVoxels[1].second, chunks);
-      const DistVoxel v_011 = getDistVoxelFromMap(cornerVoxels[2].first, cornerVoxels[2].second, chunks);
-      const DistVoxel v_111 = getDistVoxelFromMap(cornerVoxels[3].first, cornerVoxels[3].second, chunks);
-      const DistVoxel v_110 = getDistVoxelFromMap(cornerVoxels[4].first, cornerVoxels[4].second, chunks);
-      const DistVoxel v_100 = getDistVoxelFromMap(cornerVoxels[5].first, cornerVoxels[5].second, chunks);
-      const DistVoxel v_010 = getDistVoxelFromMap(cornerVoxels[6].first, cornerVoxels[6].second, chunks);
-      const DistVoxel v_101 = getDistVoxelFromMap(cornerVoxels[7].first, cornerVoxels[7].second, chunks);
+    const float x_0 = startPos(0);
+    const float y_0 = startPos(1);
+    const float z_0 = startPos(2);
+    const float x_1 = endPos(0);
+    const float y_1 = endPos(1);
+    const float z_1 = endPos(2);
 
-     /* clock_t end = clock();
-      double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-      printf("\n \n  Time needed for searching corner voxels: %f  ms  \n \n", elapsed_secs*1000);*/
+    const DistVoxel* v_000 = sourceChunkManager.GetDistanceVoxel(startPos);
+    const DistVoxel* v_001 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(0, 0, resolution));
+    const DistVoxel* v_011 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(0, resolution, resolution));
+    const DistVoxel* v_111 = sourceChunkManager.GetDistanceVoxel(endPos);
+    const DistVoxel* v_110 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(resolution, resolution, 0));
+    const DistVoxel* v_100 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(resolution, 0, 0));
+    const DistVoxel* v_010 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(0, resolution, 0));
+    const DistVoxel* v_101 = sourceChunkManager.GetDistanceVoxel(startPos + Vec3(resolution, 0, resolution));
 
-      float xd = (x - x_0) / (x_1 - x_0);
-      float yd = (y - y_0) / (y_1 - y_0);
-      float zd = (z - z_0) / (z_1 - z_0);
+    if(!v_000 || !v_001 || !v_011 || !v_111 || !v_110 || !v_100 || !v_010 || !v_101)
+    {
+      return false;
+    }
 
-      {
-          float sdf_00 = v_000.GetSDF() * (1 - xd) + v_100.GetSDF() * xd;
-          float sdf_10 = v_010.GetSDF() * (1 - xd) + v_110.GetSDF() * xd;
-          float sdf_01 = v_001.GetSDF() * (1 - xd) + v_101.GetSDF() * xd;
-          float sdf_11 = v_011.GetSDF() * (1 - xd) + v_111.GetSDF() * xd;
-          float sdf_0 = sdf_00 * (1 - yd) + sdf_10 * yd;
-          float sdf_1 = sdf_01 * (1 - yd) + sdf_11 * yd;
-          voxel->SetSDF(sdf_0 * (1 - zd) + sdf_1 * zd);
-      }
+    float xd = (x - x_0) / (x_1 - x_0);
+    float yd = (y - y_0) / (y_1 - y_0);
+    float zd = (z - z_0) / (z_1 - z_0);
 
-      {
-          float weight_00 = v_000.GetWeight() * (1 - xd) + v_100.GetWeight() * xd;
-          float weight_10 = v_010.GetWeight() * (1 - xd) + v_110.GetWeight() * xd;
-          float weight_01 = v_001.GetWeight() * (1 - xd) + v_101.GetWeight() * xd;
-          float weight_11 = v_011.GetWeight() * (1 - xd) + v_111.GetWeight() * xd;
-          float weight_0 = weight_00 * (1 - yd) + weight_10 * yd;
-          float weight_1 = weight_01 * (1 - yd) + weight_11 * yd;
-          voxel->SetWeight(weight_0 * (1 - zd) + weight_1 * zd);
-      }
+    {
+      float sdf_00 = v_000->GetSDF() * (1 - xd) + v_100->GetSDF() * xd;
+      float sdf_10 = v_010->GetSDF() * (1 - xd) + v_110->GetSDF() * xd;
+      float sdf_01 = v_001->GetSDF() * (1 - xd) + v_101->GetSDF() * xd;
+      float sdf_11 = v_011->GetSDF() * (1 - xd) + v_111->GetSDF() * xd;
+      float sdf_0 = sdf_00 * (1 - yd) + sdf_10 * yd;
+      float sdf_1 = sdf_01 * (1 - yd) + sdf_11 * yd;
+      voxel->SetSDF(sdf_0 * (1 - zd) + sdf_1 * zd);
+    }
 
-      return true;
+    {
+      float weight_00 = v_000->GetWeight() * (1 - xd) + v_100->GetWeight() * xd;
+      float weight_10 = v_010->GetWeight() * (1 - xd) + v_110->GetWeight() * xd;
+      float weight_01 = v_001->GetWeight() * (1 - xd) + v_101->GetWeight() * xd;
+      float weight_11 = v_011->GetWeight() * (1 - xd) + v_111->GetWeight() * xd;
+      float weight_0 = weight_00 * (1 - yd) + weight_10 * yd;
+      float weight_1 = weight_01 * (1 - yd) + weight_11 * yd;
+      voxel->SetWeight(weight_0 * (1 - zd) + weight_1 * zd);
+    }
+
+    return true;
   }
-
 
   Point3 Chisel::getVoxelCoordinates(VoxelID id, Eigen::Vector3i chunkSize)
   {
@@ -446,92 +417,4 @@ namespace chisel
 
     return Point3(x,y,z);
   }
-
-
-  void Chisel::getVoxelCornerIDs(std::vector<std::pair<ChunkID, VoxelID>>* cornerVoxel, const ChunkID& originalID, const Point3& originalVoxelCoords, const Eigen::Vector3i& chunkSize)
-  {
-    cornerVoxel->at(0) = fixVoxelIDs(originalID, originalVoxelCoords, chunkSize);
-    cornerVoxel->at(1) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(0,0,1), chunkSize);
-    cornerVoxel->at(2) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(0,1,1), chunkSize);
-    cornerVoxel->at(3) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(1,1,1), chunkSize);
-    cornerVoxel->at(4) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(1,1,0), chunkSize);
-    cornerVoxel->at(5) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(1,0,0), chunkSize);
-    cornerVoxel->at(6) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(0,1,0), chunkSize);
-    cornerVoxel->at(7) = fixVoxelIDs(originalID, originalVoxelCoords+Point3(1,0,1), chunkSize);
-  }
-
-
-  std::pair<ChunkID, VoxelID> Chisel::fixVoxelIDs(const ChunkID& originalID, const Point3& originalVoxelCoords, const Eigen::Vector3i& chunkSize)
-  {
-    Point3 voxelCoords(originalVoxelCoords);
-    ChunkID chunkID(originalID);
-
-    //check x chunk id and voxelCoordinate
-    if(originalVoxelCoords(0)<0)
-    {
-      voxelCoords(0) = chunkSize(0) + originalVoxelCoords(0);
-      chunkID(0) -= 1;
-    }
-    else if (originalVoxelCoords(0) >= chunkSize(0))
-    {
-      voxelCoords(0) = originalVoxelCoords(0) - chunkSize(0);
-      chunkID(0) += 1;
-    }
-
-    //check y chunk id and voxelCoordinate
-    if(originalVoxelCoords(1)<0)
-    {
-      voxelCoords(1) = chunkSize(1) + originalVoxelCoords(1);
-      chunkID(1) -= 1;
-    }
-    else if (originalVoxelCoords(1) >= chunkSize(1))
-    {
-      voxelCoords(1) = originalVoxelCoords(1) - chunkSize(1);
-      chunkID(1) += 1;
-    }
-
-    //check z chunk id and voxelCoordinate
-    if(originalVoxelCoords(2)<0)
-    {
-      voxelCoords(2) = chunkSize(2) + originalVoxelCoords(2);
-      chunkID(2) -= 1;
-    }
-    else if (originalVoxelCoords(2) >= chunkSize(2))
-    {
-      voxelCoords(2) = originalVoxelCoords(2) - chunkSize(2);
-      chunkID(2) += 1;
-    }
-
-    VoxelID voxelID = voxelCoords(0) * voxelCoords(1) * voxelCoords(2);
-
-    if (voxelID <0 || voxelID > chunkSize(0)*chunkSize(1)*chunkSize(2))
-    {
-      printf("Chunk Size: %d %d %d \n", chunkSize(0), chunkSize(1), chunkSize(2)) ;
-      printf("original coords: %d %d %d \n", originalVoxelCoords(0), originalVoxelCoords(1), originalVoxelCoords(2));
-      printf("invalid coords: %d %d %d \n", voxelCoords(0), voxelCoords(1), voxelCoords(2));
-
-      printf("original chunk ID: %d %d %d \n", originalID(0), originalID(1), originalID(2));
-      printf("invalid chunkID: %d %d %d \n", chunkID(0), chunkID(1), chunkID(2));
-
-    }
-
-    return std::pair<ChunkID, VoxelID>(chunkID, voxelID);
-  }
-
-  DistVoxel Chisel::getDistVoxelFromMap(const ChunkID chunkID, const VoxelID voxelID, const ChunkMap chunks)
-  {
-     ChunkMap::const_iterator chunk_it = chunks.find (chunkID);
-
-     if ( chunk_it != chunks.end() )
-     {
-       return chunk_it->second->GetDistVoxel(voxelID);
-     }
-     else
-     {
-      DistVoxel d;
-      return d;
-     }
-
-  }
-
 } // namespace chisel 
